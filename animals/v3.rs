501fd1bc347c8e9381c6a69638d058897627c8f1
Colton <colt.robertson@toyotaconnected.com>
2024-01-18 23:43:54 -0600

    init

---
+++ animals/v3.rs
@@ -0,0 +1,5 @@
+use crate::sounds::Noise;
+
+pub fn make_sound() -> Noise {
+    Noise::Moo
+}
---
+++ main.rs
@@ -0,0 +1,6 @@
+mod animals;
+pub mod sounds;
+
+fn main() {
+    println!("{:?}", animals::make_sound());
+}
